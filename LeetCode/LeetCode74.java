0cbdd621647f69fe3c3b3970d63867321970f3ac
Paranshu Singhal <paranshu.singhal@gmail.com>
2019-10-29 19:24:53 -0700

    adding problem 74

---
+++ LeetCode/LeetCode74.java
@@ -0,0 +1,32 @@
+/**
+ * LeetCode74
+ */
+public class LeetCode74 {
+
+    static boolean searchMatrix(int[][] matrix, int target) {
+        
+        int x = matrix.length;
+        if(x==0) return false;
+        
+        int y = matrix[0].length;
+        if(y==0) return false;
+
+        for(int i=0; i<x; i++){
+            if(target == matrix[i][0]) return true;
+            if(target == matrix[i][y-1]) return true;
+            if(target > matrix[i][0] && target < matrix[i][y-1]) return BinarySearch(matrix[i], 0, y-1, target);
+        }
+        return false;
+    }
+
+    static boolean BinarySearch(int[] array, int start, int end, int target){
+        
+        int middle = (start+end)/2;
+
+        if(target == array[middle]) return true;
+        if(start == end && target != array[middle]) return false;
+
+        if(target < array[middle]) return BinarySearch(array, start, middle-1, target);
+        return BinarySearch(array, middle+1, end, target);
+    }
+}
