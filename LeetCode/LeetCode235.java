c41473601f376d56b99e7a1cbba512f5269243ad
Paranshu Singhal <paranshu.singhal@gmail.com>
2019-11-10 16:50:34 -0800

    Adding leet 235

--- LeetCode/LeetCode235.java
+++ LeetCode/LeetCode235.java
@@ -12,7 +12,7 @@ class LeetCode235 {
         
         if(p.val <= root.val && q.val >= root.val || p.val >= root.val && q.val <= root.val) return root;
         if(p.val < root.val && q.val < root.val) return lowestCommonAncestor(root.left, p, q);
-            
+        
         return lowestCommonAncestor(root.right, p, q);
     }
 }
