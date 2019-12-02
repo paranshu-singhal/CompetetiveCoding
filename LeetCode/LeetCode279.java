c951c89cb0939f12274b48913114d9728de26924
Paranshu Singhal <paranshu.singhal@gmail.com>
2019-12-01 20:17:36 -0800

    Optimizing 279

--- LeetCode/LeetCode279.java
+++ LeetCode/LeetCode279.java
@@ -1,25 +1,33 @@
 class Solution {
     public int numSquares(int n) {
         
-        List<Integer> squares = new ArrayList();
+//         List<Integer> squares = new ArrayList();
         
-        int i=1;
-        while((i*i)<=n){
-            squares.add((i*i));
-            i++;
-        }
+//         int i=1;
+//         while((i*i)<=n){
+//             squares.add((i*i));
+//             i++;
+//         }
         
         int[] dp = new int[n+1];
         
-        for(i=1;i<=n;i++){
+        // for(i=1;i<=n;i++){
+        //     dp[i] = Integer.MAX_VALUE;
+        //     for(int j=0;j<squares.size();j++){
+        //         if(i-squares.get(j) >=0){
+        //             dp[i] = Math.min(dp[i], dp[i-squares.get(j)] + 1);
+        //         }
+        //     }
+        // }
+        
+        for(int i=1; i<=n;i++){
             dp[i] = Integer.MAX_VALUE;
-            for(int j=0;j<squares.size();j++){
-                if(i-squares.get(j) >=0){
-                    dp[i] = Math.min(dp[i], dp[i-squares.get(j)] + 1);
-                }
+            
+            for(int j=1; j<=Math.sqrt(i);j++){
+                dp[i] = Math.min(dp[i], dp[i-(j*j)] + 1);
             }
         }
         return dp[n];
-        //return 0;
+        
     }
 }
