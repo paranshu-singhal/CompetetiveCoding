a9530decfec0c2916ab390d3fd98fa4abe7ab1a9
Paranshu Singhal <paranshu.singhal@gmail.com>
2020-04-12 19:59:41 -0700

    Solving dp problem 1223

---
+++ LeetCode/LeetCode1223.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int dieSimulator(int n, int[] rollMax) {
+        int len = rollMax.length;
+        
+        if(n==1) return 6;
+        
+        int[][] dp = new int[n][6];
+        int[] total = new int[n];
+        
+        for(int j=0; j<6; j++) dp[0][j] = 1;
+        total[0] = 6;
+        
+        int mod = 1000000007;
+        
+        for(int i=1; i<n; i++){
+            int sum = 0;
+            for(int j=0; j<6; j++){
+                dp[i][j] = total[i-1];
+                
+                if(i - rollMax[j] >= 1){
+                    dp[i][j] = ((dp[i][j] - total[i - rollMax[j] - 1] + dp[i-rollMax[j]-1][j]) % mod + mod)%mod;
+                } else if(i - rollMax[j] == 0){
+                    dp[i][j]--;
+                }
+                sum = (sum + dp[i][j]) % mod;
+            }
+            total[i] = sum;
+            //System.out.println(Arrays.toString(dp[i]));
+        }
+        
+        // int res=0;
+        // for(int i=0; i<6; i++){
+        //     res += dp[n-1][i];
+        // }
+        return total[n-1];
+    }
+}
